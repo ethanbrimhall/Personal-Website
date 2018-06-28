87a7648e58452888514efef3bb8782a3eed3e9a0
Ethan <ethannbrimhall@gmail.com>
2018-06-28 16:19:36 -0400

    Created Files

---
+++ server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+
+var app = express();
+var server = http.createServer(app);
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+	res.render('pages/index');
+});
+
+server.listen(8080, () => {
+   console.log('Server Started on port 8080'); 
+});
